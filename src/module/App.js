b60acf5574ed351009b19a7c47201719a5884f94
杨欣 <yangxin2@yy.com>
2019-05-04 16:35:39 +0800

    refactor: 重构种子项目

--- src/module/App.js
+++ src/module/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import './App.css';
+import TodoList from './TodoList'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello World!!!
+        <TodoList/>
       </div>
     );
   }
---
+++ src/module/TodoList/index.js
@@ -0,0 +1,12 @@
+import React ,{Component} from 'react'
+
+class TodoList extends Component {
+  state = {  }
+  render() { 
+    return ( 
+      <div>todoList</div>
+     )
+  }
+}
+ 
+export default TodoList;
