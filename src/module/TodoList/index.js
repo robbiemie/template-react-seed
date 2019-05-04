384a1da65a1cafc4ce996c67f21fff2d3b463908
杨欣 <yangxin2@yy.com>
2019-05-04 17:11:08 +0800

    feat: 新增方法/事件绑定

--- src/module/TodoList/index.js
+++ src/module/TodoList/index.js
@@ -1,17 +1,48 @@
 import React ,{Component,Fragment} from 'react'
+import './index.css'
 
 class TodoList extends Component {
-  state = {  }
+  constructor(){
+    super()
+    this.state = {
+      target: '',
+      list: []
+    }
+  }
   render() { 
     return ( 
       <Fragment>
+        <input 
+          value={this.state.target} // 绑定数据
+          onChange={this.handleInput.bind(this)} // 绑定方法
+        />
+        <button onClick={this.handleSubmit.bind(this)}>提交</button>
         <ul>
-          <li>1</li>
-          <li>2</li>
+          {
+            this.state.list.map((item,index)=>{
+              return <li key={index}>{item.value}</li>
+            })
+          }
         </ul>
-        <button>提交</button>
       </Fragment>
      )
+  }
+  handleInput(e){
+    // 修改state中的数据
+    console.log('e',e.target.value)
+    this.setState({
+      target: e.target.value
+    })
+  }
+  handleSubmit(e){
+    console.log('e',e)
+    const list = this.state.list
+    list.push({
+      value: this.state.target
+    })
+    this.setState({
+      list
+    })
   }
 }
  
