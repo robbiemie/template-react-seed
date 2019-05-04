7c2f32bfdc6f58fccace5b1ea7f7029b848310e5
杨欣 <yangxin2@yy.com>
2019-05-04 16:41:02 +0800

    feat: 使用 Fragment 占位

--- src/module/TodoList/index.js
+++ src/module/TodoList/index.js
@@ -1,10 +1,16 @@
-import React ,{Component} from 'react'
+import React ,{Component,Fragment} from 'react'
 
 class TodoList extends Component {
   state = {  }
   render() { 
     return ( 
-      <div>todoList</div>
+      <Fragment>
+        <ul>
+          <li>1</li>
+          <li>2</li>
+        </ul>
+        <button>提交</button>
+      </Fragment>
      )
   }
 }
