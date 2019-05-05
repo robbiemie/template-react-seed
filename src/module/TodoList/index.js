77b8c73cff206763373f109689807a2a517aa7a0
yang657850144 <yang657850144@gmail.com>
2019-05-05 21:07:24 +0800

    feat: 使用redux

--- src/module/TodoList/index.js
+++ src/module/TodoList/index.js
@@ -6,14 +6,16 @@ import './index.css'
 class TodoList extends Component {
   constructor(){
     super()
-    this.state = store.getState()
-    console.log('data',store.getState())
     this.bindMethods()
+    this.state = store.getState()
+    store.subscribe(this.handleStoreChange)
+    // console.log('data',store.getState())
   }
   bindMethods(){
-    this.handleInput = this.handleInput.bind(this)
+    this.handleInputChange = this.handleInputChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
     this.handleDelete = this.handleDelete.bind(this)
+    this.handleStoreChange = this.handleStoreChange.bind(this)
   }
   render() { 
     return ( 
@@ -23,7 +25,7 @@ class TodoList extends Component {
           placeholder="please input words"
           className="search"
           value={this.state.target} // 绑定数据
-          onChange={this.handleInput} // 绑定方法
+          onChange={this.handleInputChange} // 绑定方法
         />
         <button onClick={this.handleSubmit}>submit</button>
         <ul>
@@ -42,35 +44,40 @@ class TodoList extends Component {
       )
     })
   }
-  handleInput(e){
-    // 修改state中的数据
-    console.log('input',e.target.value)
-    // 异步set
-    let val = e.target.value
-    this.setState(_=>({
-      target: val
-    }))
+  handleStoreChange(data){
+    this.setState(store.getState())
+  }
+  handleInputChange(e){
+    const action = {
+      type: 'change_input_value',
+      value: e.target.value
+    }
+    store.dispatch(action)
   }
   handleSubmit(e){
     // 异步set
-    this.setState(prevState=>({
-      target: '',
-      list: [...prevState.list,{value:prevState.target}]
-    }))
-    console.log('submit',this.state.list)
+    const action = {
+      type: 'submit_input_value'
+    }
+    store.dispatch(action)
   }
 
   handleDelete(index){
     console.log('delete',index)
     // state 不允许直接被修改 
-    // Immutable 
-    this.setState(prevState=>{
-      const list = [...prevState.list]
-      list.splice(index,1)
-      return {
-        list
-      }
-    })
+    const action = {
+      type: 'delete_index_value',
+      value: index
+    }
+    store.dispatch(action)
+    // // Immutable 
+    // this.setState(prevState=>{
+    //   const list = [...prevState.list]
+    //   list.splice(index,1)
+    //   return {
+    //     list
+    //   }
+    // })
   }
 }
  
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -2,6 +2,25 @@ const defaultData = {
   target: '',
   list: []
 }
+// reducer 不可以修改 state 值
 export default (state = defaultData, actions) => {
+  console.log('reducer', state, actions)
+  if (actions.type === 'change_input_value') {
+    const newState = JSON.parse(JSON.stringify(state))
+    newState.target = actions.value
+    return newState
+  } 
+  if (actions.type === 'submit_input_value') {
+    const newState = JSON.parse(JSON.stringify(state))
+    newState.list.push({value:newState.target})
+    newState.target = ''
+    return newState
+  }
+  if (actions.type === 'delete_index_value') {
+    const newState = JSON.parse(JSON.stringify(state))
+    newState.list.splice(actions.value,1)
+    // newState.target = ''
+    return newState
+  }
   return state
 }
